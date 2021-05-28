867278579264931b50682584264841ca72fcc4e8
anbreaker <antunez19@gmail.com>
2021-05-28 08:28:25 +0200

    feature: init page React basic

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavbarComponent } from './ui/NavbarComponent';
+
+export const App = () => {
+  return (
+    <div>
+      <NavbarComponent />
+      <h1>Meep</h1>
+    </div>
+  );
+};
